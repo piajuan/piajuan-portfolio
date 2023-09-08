e659aaf763d839f0649958ed6d2358877769cda6
Pia Juan <piajuan.dev@gmail.com>
2023-09-08 17:28:41 +0800

    updated header behavior

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState , useEffect} from 'react'
 import { Link } from 'react-router-dom'
 
+function useScrollDirection() {
+  const [scrollDirection, setScrollDirection] = useState(null);
+
+  useEffect(() => {
+    let lastScrollY = window.pageYOffset;
+
+    const updateScrollDirection = () => {
+      const scrollY = window.pageYOffset;
+      const direction = scrollY > lastScrollY ? "down" : "up";
+      if (direction !== scrollDirection && (scrollY - lastScrollY > 10 || scrollY - lastScrollY < -10)) {
+        setScrollDirection(direction);
+      }
+      lastScrollY = scrollY > 0 ? scrollY : 0;
+    };
+    window.addEventListener("scroll", updateScrollDirection); // add event listener
+    return () => {
+      window.removeEventListener("scroll", updateScrollDirection); // clean up
+    }
+  }, [scrollDirection]);
+
+  return scrollDirection;
+};
+
 const Header = () => {
+  const scrollDirection = useScrollDirection();
+  const [isHeaderOnTop, setIsHeaderOnTop] = useState(true);
+
+  useEffect(()=> {
+    window.onscroll = () =>
+    window.pageYOffset === 0 ? setIsHeaderOnTop(true) : setIsHeaderOnTop(false);
+
+    return () => (window.onscroll = null);
+  })
+
   return (
-    <div className='p-4 fixed top-0 left-0 w-full'>
+    <header className={`p-4 fixed top-0 left-0 w-full z-50 ${ scrollDirection === "down" ? "-top-[56px]" : "top-0"} ${!isHeaderOnTop ? "header--onScroll" : "bg-transparent" } transition-all duration-500`}>
         <div className="container mx-auto">
             <nav className="flex justify-between items-center">
                 <Link to={"/"} className='font-tertiary uppercase dark:text-white'>Pia Juan</Link>
@@ -14,7 +47,7 @@ const Header = () => {
                 </ul>
             </nav>
         </div>
-    </div>
+    </header>
   )
 }
 
