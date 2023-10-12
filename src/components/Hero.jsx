33abacaacedfaee4ced61a716e76fb7ad03ed96b
Pia Juan <piajuan.dev@gmail.com>
2023-10-12 19:45:40 +0800

    fixed noise img not showing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
             {/* end: blobs */}
             <div className='absolute w-full h-full p-4 flex-auto flex flex-col justify-end md:justify-center items-center bg-white/[0.2] backdrop-blur-[180px] border-2 border-solid border-white/[0.18] box-shadow-hero'>
                 {/* noise effect */}
-                <div className='content-[""] absolute left-0 top-0 block w-full h-full mix-blend-overlay z-0 bg-[url(./src/assets/noise.png)] opacity-80 pointer-events-none'></div>
+                <div className='content-[""] absolute left-0 top-0 block w-full h-full mix-blend-overlay z-0 bg-[url(/noise.png)] opacity-80 pointer-events-none'></div>
                 {/* content */}
                 <h1 className='gs-hero__content text-7xl text-left md:text-[7vw] lg:max-w-[70vw] mx-auto font-primary md:text-center leading-none'>I’m Pia  —frontend developer & designer</h1>
                 {/* <p className='gs-hero__content  max-w-[500px] mt-5 mx-auto mb-14 md:mb-0 text-left md:text-center'>currently based in Metro Manila, Philippines with two years of experience -- passionate about delivering user-oriented interface designs</p> */}
